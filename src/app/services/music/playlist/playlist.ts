60f0d0dd839a8c20b4aea3269fe423d93134afe2
bastok <bfromager@gmail.com>
2018-11-03 22:36:20 +0100

    Playlist NextMethod (Forward/Backward/Random)

--- src/app/services/music/playlist/playlist.ts
+++ src/app/services/music/playlist/playlist.ts
@@ -2,7 +2,11 @@ import {Injectable} from "@angular/core";
 import {FileService} from "../../files/file.service";
 import {Mp3} from "../mp3.interface";
 
-//todo: delete wrong files
+export enum NextMethod {
+    Forward = 0,
+    Backward = 1,
+    Random = 2,
+}
 
 @Injectable({
     providedIn: 'root',
@@ -25,7 +29,7 @@ export interface PlaylistInterface {
 export class Playlist {
     private name: string;
     private list: Mp3[] = [];
-    private currentIndex;
+    private currentIndex = 0;
 
     constructor(private fileService: FileService) {
     }
@@ -54,29 +58,40 @@ export class Playlist {
     loadFromPlaylistInterface(playlistInterface: PlaylistInterface) {
         this.name = playlistInterface.name;
         this.list = playlistInterface.list;
+        this.initPlaylist();
     }
 
     initPlaylist() {
         this.currentIndex = 0;
     }
 
-    public getNextMp3(): Promise<Mp3> {
+    public getNextMp3(method: NextMethod = NextMethod.Forward): Promise<Mp3> {
         return new Promise((resolve, reject) => {
             if (this.list.length == 0) {
                 reject("Playlist is empty");
             }
 
-            if ((this.currentIndex) >= this.list.length) {
-                this.initPlaylist();
+            if ((method == NextMethod.Forward) && (this.currentIndex >= this.list.length)) {
+                this.currentIndex = 0;
+            } else
+            if ((method == NextMethod.Backward) && (this.currentIndex < 0)) {
+                this.currentIndex = this.list.length - 1;
             }
-            let nextMp3 = this.list[this.currentIndex++];
+
+            let nextMp3:Mp3;
+            if (method == NextMethod.Random)
+                nextMp3 = this.list[Math.floor(Math.random() * this.list.length)];
+            else if (method == NextMethod.Backward)
+                nextMp3 = this.list[this.currentIndex--];
+            else
+                nextMp3 = this.list[this.currentIndex++];
 
             this.fileService.exists(nextMp3.path).then((result)=>{
                 console.log('file.exists : ', result);
                 resolve(nextMp3);
             }).catch((error)=>{
                 console.log('file.exists : error ', error);
-                this.getNextMp3()
+                this.getNextMp3(method)
                     .then((mp3:Mp3)=>{
                         resolve(mp3);
                     })
