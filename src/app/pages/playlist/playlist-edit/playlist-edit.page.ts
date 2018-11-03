5750b6a0016d3782e9f1f7a5745e67c1fffac78b
bastok <bfromager@gmail.com>
2018-11-03 09:19:05 +0100

    Rename playlist

--- src/app/pages/playlist/playlist-edit/playlist-edit.page.ts
+++ src/app/pages/playlist/playlist-edit/playlist-edit.page.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {Router} from "@angular/router";
 import {PlaylistEditComponent} from "../../../services/music/playlist/components/playlistEdit.component";
 import {Playlist} from "../../../services/music/playlist/playlist";
+import {Subscription} from "rxjs/index";
 
 @Component({
     selector: 'app-playlist-edit',
@@ -12,14 +13,27 @@ export class PlaylistEditPage implements OnInit {
     @ViewChild(PlaylistEditComponent) playlistEdit: PlaylistEditComponent;
 
     public playlist: Playlist;
+    public playlistName: string;
+    private playlistAddMp3Sub: Subscription;
 
-    constructor(private router: Router) { }
+    constructor(private router: Router) {
+    }
 
     ngOnInit() {
         this.playlist = this.playlistEdit.playlist;
+        this.playlistName = this.playlist.getName();
+        this.playlistAddMp3Sub = this.playlistEdit.playlistAddMp3.subscribe(
+            () => {
+                this.router.navigateByUrl('/mp3-list');
+            }
+        )
     }
 
-    mp3(){
-        this.router.navigateByUrl('/mp3-list');
+    ngOnDestroy() {
+        this.playlistAddMp3Sub.unsubscribe();
+    }
+
+    rename() {
+        this.playlistEdit.rename().then((name)=>{this.playlistName = name;});
     }
 }
--- src/app/services/music/playlist/components/playlistEdit.component.ts
+++ src/app/services/music/playlist/components/playlistEdit.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnDestroy, OnInit} from "@angular/core";
 import {Playlist} from "../playlist";
 import {PlaylistsService} from "../playlists/playlists.service";
-// import {Subject} from "rxjs/index";
+import {Subject} from "rxjs/index";
+import {AlertController} from "@ionic/angular";
+
 @Component({
     selector: 'playlist-edit-component',
     templateUrl: 'playlistEdit.component.html',
@@ -9,9 +11,9 @@ import {PlaylistsService} from "../playlists/playlists.service";
 export class PlaylistEditComponent implements OnInit, OnDestroy {
 
     public playlist : Playlist = null;
-    // public playlistClick: Subject<Playlist> = new Subject<Playlist>();
+    public playlistAddMp3: Subject<void> = new Subject<void>();
 
-    constructor(private playlistsService: PlaylistsService) {
+    constructor(private playlistsService: PlaylistsService, private alertController: AlertController) {
         this.playlist = this.playlistsService.getEditingPlaylist();
     }
 
@@ -20,7 +22,52 @@ export class PlaylistEditComponent implements OnInit, OnDestroy {
     ngOnDestroy() {
     }
 
-    // itemClick(playlist: Playlist){
-    //     this.playlistClick.next(playlist);
-    // }
+    public rename(): Promise<string> {
+        return new Promise((resolve, reject) => {
+            this.alertController.create({
+                header: 'Nom de la playlist',
+                inputs: [
+                    {
+                        name: 'playLitsName',
+                        type: 'text',
+                        // placeholder: 'Placeholder 1',
+                        value: this.playlist.getName(),
+                    }
+                ],
+                buttons: [
+                    {
+                        text: 'Cancel',
+                        role: 'cancel',
+                        cssClass: 'secondary',
+                        // handler: () => {
+                        //     console.log('Confirm Cancel');
+                        // }
+                    },
+                    {
+                        text: 'Ok',
+                        handler: (inputs) => {
+                            let playlistName = inputs.playLitsName;
+                            this.playlist.setName(playlistName);
+                            this.playlistsService.save();
+                            resolve(playlistName);
+                        }
+                    }
+                ]
+            })
+                .then((alertForm) => {
+                        alertForm.present().then(() => {
+                            // const firstInput: any = document.querySelector('ion-alert input');
+                            // firstInput.focus();
+                            // return;
+                        });
+                    }
+                );
+        })
+    }
+
+    addMp3() {
+        this.playlistAddMp3.next();
+    }
+
+
 }
