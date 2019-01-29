54de06ad2cd8fac9892d9cb7bad328fa916ead08
bastok <bfromager@gmail.com>
2019-01-29 11:33:05 +0100

    Add Seance page

---
+++ src/app/pages/seance/seance.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seance',
+  templateUrl: './seance.page.html',
+  styleUrls: ['./seance.page.scss'],
+})
+export class SeancePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
